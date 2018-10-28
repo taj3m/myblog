97ea1056a410ae59ceb6d14d549b3000a804ed78
taj3m <uzoqov@taj3m.ru>
2018-10-28 22:34:40 +0300

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,127 @@
+@extends('admin.layouts.app_admin')
+@section('content')
+<div class="container">
+	<div class="container-fluid">
+		<div class="animated fadeIn">
+			<div class="row">
+			<div class="col-sm-6 col-lg-3">
+			
+<div class="card text-white bg-primary">
+<div class="card-body pb-0">
+<div class="btn-group float-right">
+<button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<i class="icon-settings"></i>
+</button>
+<div class="dropdown-menu dropdown-menu-right">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</div>
+<div class="text-value">9.823</div>
+<div>Members online</div>
+</div>
+<div class="chart-wrapper mt-3 mx-3" style="height:70px;"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+<canvas class="chart chartjs-render-monitor" id="card-chart1" height="210" width="798" style="display: block; width: 266px; height: 70px;"></canvas>
+<div id="card-chart1-tooltip" class="chartjs-tooltip top" style="opacity: 0; left: 193.558px; top: 123.889px;"><div class="tooltip-header"><div class="tooltip-header-item">June</div></div><div class="tooltip-body"><div class="tooltip-body-item"><span class="tooltip-body-item-color" style="background-color: rgb(0, 165, 224);"></span><span class="tooltip-body-item-label">My First dataset</span><span class="tooltip-body-item-value">55</span></div></div></div></div>
+</div>
+</div>
+			<div class="col-sm-6 col-lg-3">
+			
+<div class="card text-white bg-primary">
+<div class="card-body pb-0">
+<div class="btn-group float-right">
+<button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<i class="icon-settings"></i>
+</button>
+<div class="dropdown-menu dropdown-menu-right">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</div>
+<div class="text-value">9.823</div>
+<div>Members online</div>
+</div>
+<div class="chart-wrapper mt-3 mx-3" style="height:70px;"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+<canvas class="chart chartjs-render-monitor" id="card-chart1" height="210" width="798" style="display: block; width: 266px; height: 70px;"></canvas>
+<div id="card-chart1-tooltip" class="chartjs-tooltip top" style="opacity: 0; left: 193.558px; top: 123.889px;"><div class="tooltip-header"><div class="tooltip-header-item">June</div></div><div class="tooltip-body"><div class="tooltip-body-item"><span class="tooltip-body-item-color" style="background-color: rgb(0, 165, 224);"></span><span class="tooltip-body-item-label">My First dataset</span><span class="tooltip-body-item-value">55</span></div></div></div></div>
+</div>
+</div>
+			<div class="col-sm-6 col-lg-3">
+			
+<div class="card text-white bg-primary">
+<div class="card-body pb-0">
+<div class="btn-group float-right">
+<button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<i class="icon-settings"></i>
+</button>
+<div class="dropdown-menu dropdown-menu-right">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</div>
+<div class="text-value">9.823</div>
+<div>Members online</div>
+</div>
+<div class="chart-wrapper mt-3 mx-3" style="height:70px;"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+<canvas class="chart chartjs-render-monitor" id="card-chart1" height="210" width="798" style="display: block; width: 266px; height: 70px;"></canvas>
+<div id="card-chart1-tooltip" class="chartjs-tooltip top" style="opacity: 0; left: 193.558px; top: 123.889px;"><div class="tooltip-header"><div class="tooltip-header-item">June</div></div><div class="tooltip-body"><div class="tooltip-body-item"><span class="tooltip-body-item-color" style="background-color: rgb(0, 165, 224);"></span><span class="tooltip-body-item-label">My First dataset</span><span class="tooltip-body-item-value">55</span></div></div></div></div>
+</div>
+</div>
+			<div class="col-sm-6 col-lg-3">
+			
+<div class="card text-white bg-primary">
+<div class="card-body pb-0">
+<div class="btn-group float-right">
+<button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<i class="icon-settings"></i>
+</button>
+<div class="dropdown-menu dropdown-menu-right">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</div>
+<div class="text-value">9.823</div>
+<div>Members online</div>
+</div>
+<div class="chart-wrapper mt-3 mx-3" style="height:70px;"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+<canvas class="chart chartjs-render-monitor" id="card-chart1" height="210" width="798" style="display: block; width: 266px; height: 70px;"></canvas>
+<div id="card-chart1-tooltip" class="chartjs-tooltip top" style="opacity: 0; left: 193.558px; top: 123.889px;"><div class="tooltip-header"><div class="tooltip-header-item">June</div></div><div class="tooltip-body"><div class="tooltip-body-item"><span class="tooltip-body-item-color" style="background-color: rgb(0, 165, 224);"></span><span class="tooltip-body-item-label">My First dataset</span><span class="tooltip-body-item-value">55</span></div></div></div></div>
+</div>
+</div>
+			
+			</div>
+			<br>
+			<div class="row">
+				
+			<div class="col-sm-6 ">
+<div class="card">
+<div class="card-header">Card title</div>
+<div class="card-body">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+ex ea commodo consequat.</div>
+</div>
+</div>
+			<div class="col-sm-6 ">
+<div class="card">
+<div class="card-header">Card title</div>
+<div class="card-body">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+ex ea commodo consequat.</div>
+</div>
+</div>
+			
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+@endsection
